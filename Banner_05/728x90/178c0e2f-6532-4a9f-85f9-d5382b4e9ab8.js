ad0b1207c19ce720db9f3a406772a9ec5c0d51d4
lik571 <54274837+lik571@users.noreply.github.com>
2022-09-20 12:36:01 +0530

    banner_05

---
+++ Banner_05/728x90/178c0e2f-6532-4a9f-85f9-d5382b4e9ab8.js
@@ -0,0 +1,43 @@
+function animation_728x90() {
+
+    //Endframe
+    function endFrame() {
+      console.log('endFrame()');
+    }
+
+    //Timeline
+    var loopCounter = 0;
+    var tl = gsap.timeline({ delay: 0, repeat: globalTimeline.repeat, repeatDelay: globalTimeline.repeatDelay, onComplete: endFrame });
+
+    tl.addLabel("start")
+      .to(element.banner, 0, { opacity: 1 }, "start")
+      .set(element.copy_1, { x: 100, opacity: 0 })
+      .set(element.copy_sub_2,{ x: 100, opacity: 0 })
+      .set(element.scrollbar_container, { bottom: -80, opacity: 0  })
+      .set(element.logo, { opacity: 0 })
+      .set(element.thirdFrame, { x: -100, opacity: 0  })
+      .set(element.cta_1, { opacity: 0 })
+
+      .addLabel("phase_1", "+=0")
+        .to(element.copy_1, 1, { opacity: 1, x: 0, ease: Power3.easeOut }, "phase_1")
+        .to(element.scrollbar_container, 2, { opacity: 1, bottom: 0, ease: Power4.easeInOut}, "phase_1")
+        .to(element.logo, 0.5, { opacity: 1, ease: Power1.easeIn }, "phase_1")
+
+      .addLabel("phase_2", "+=2")
+        .to(element.copy_sub_2, 1.5, { opacity: 1, x: 0, ease: Power3.easeOut }, "phase_2")
+        .to(element.copy_1, 0.5, { opacity: 0 }, "phase_2")
+       
+      .addLabel("phase_3", "+=2")
+        .to(element.copy_sub_2, 0.5, { opacity: 0 }, "phase_3")
+        .to(element.thirdFrame, 1, { opacity: 1, x: 0, ease: Power3.easeOut}, "phase_3")
+        .to(element.logo, 0.5, { opacity: 0}, "phase_3")
+        .to(element.cta_1, 1, { opacity: 1}, "phase_3+=1")
+
+      .add(function () {
+        console.log('tl:', tl.iteration());
+        if (tl.iteration() - 1 == globalTimeline.repeat) {
+          console.log('END');
+          tl.pause()
+        }
+      });
+  }
