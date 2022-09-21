54af8a999f1e59e1657e789da1aaf89cfc3a22a8
lik571 <54274837+lik571@users.noreply.github.com>
2022-09-21 18:14:16 +0530

    Add files via upload
    Exit link update

---
+++ Banner_03/728x90/728x90/71ad854b-7a30-4a5f-9412-e5d506b88cf1.js
@@ -0,0 +1,30 @@
+function animation_300x250() {
+    console.log('file_working');
+    //Endframe
+    function endFrame() {
+      console.log('endFrame()');
+    }
+
+    //Timeline
+    var loopCounter = 0;
+    var tl = gsap.timeline({ delay: 0, repeat: globalTimeline.repeat, repeatDelay: globalTimeline.repeatDelay, onComplete: endFrame });
+
+    tl.addLabel("frame1")
+      .to([element.image_1,element.trustmark_logo,element.sc_logo,element.headline], 0, { opacity: 1 }, "frame1")
+
+    tl.addLabel("frame2","+=2")
+    .to(element.image_1, 1, { opacity: 0 }, "frame2")
+      .to(element.image_2, 2, { opacity: 1 }, "frame2")
+      .to(element.headline, 1, { opacity: 0 }, "frame2")
+      .to(element.subhead, 2, { opacity: 1 }, "frame2+=1.5")
+      .to(element.button, 1, { opacity: 0 }, "frame2+=2")
+
+      tl.addLabel("frame3","+=2")
+      .to(element.trustmark_logo, 1, { opacity: 0 }, "frame3")
+      .to(element.sc_logo, 1, { opacity: 0 }, "frame3")
+      .to(element.image_2, 1, { opacity: 0 }, "frame3+=5")
+      .to(element.headline, 1, { opacity: 0 }, "frame3")
+      .to(element.subhead, 1, { opacity: 0 }, "frame3")
+      .to(element.button, 1, { opacity: 0 }, "frame3")
+      .to(element.image_3, 1, { opacity: 1 }, "frame3")
+  }
